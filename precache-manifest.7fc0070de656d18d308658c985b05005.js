6a73eee4b79a6fc4aed5e8682de4f43a2aaef969
le11evan <le11evan@gmail.com>
2022-11-15 03:49:02 -0800

    Updates

---
+++ precache-manifest.7fc0070de656d18d308658c985b05005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bff4cfdfeee69c4810ef0753249cff",
+    "url": "/merchy/index.html"
+  },
+  {
+    "revision": "360b3a01fd125a29f4e4",
+    "url": "/merchy/static/js/2.3eb61dc9.chunk.js"
+  },
+  {
+    "revision": "694a527c48176f745ce39db359997b43",
+    "url": "/merchy/static/js/2.3eb61dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52e8f0ce2a14fa4b80a",
+    "url": "/merchy/static/js/main.04c60527.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/merchy/static/js/main.04c60527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64af980ca7602deb213e",
+    "url": "/merchy/static/js/runtime-main.1e59c262.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/merchy/static/media/commerce.422fec10.png"
+  }
+]);
